1e7d87cd01994711878c99617574ae6fd5cb1306
@hexlet/cli <support@hexlet.io>
2024-02-18 20:08:32 +0300

    submit java-oop-ru/code-generation

---
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -0,0 +1,38 @@
+package exercise;
+
+import lombok.SneakyThrows;
+import lombok.Value;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// BEGIN
+@Value
+// END
+class Car {
+    int id;
+    String brand;
+    String model;
+    String color;
+    User owner;
+
+    // BEGIN
+    @SneakyThrows
+    public String serialize() {
+        return new ObjectMapper().writeValueAsString(this);
+        //final ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            return new ObjectMapper().writeValueAsString(this);
+//        } catch (Exception e) {
+//            return "";
+//        }
+    }
+    @SneakyThrows
+    public static Car unserialize(String carJSONString) {
+        return new ObjectMapper().readValue(carJSONString, Car.class);
+//        try {
+//            return new ObjectMapper().readValue(carJSONString, Car.class);
+//        } catch (Exception e) {
+//            return null;
+//        }
+    }
+    // END
+}
